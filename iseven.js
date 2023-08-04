d25de133ceb952f742bb4d9059f28e9de1b19bfa
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-04 15:14:12 +0600

    commit

---
+++ iseven.js
@@ -0,0 +1,9 @@
+function isEvan(number){
+    const evan = number % 2
+    if(evan==0){
+        console.log("This is even number");
+    }else{
+        console.log("This is not enen number Bro");
+    }
+}
+isEvan(1244)

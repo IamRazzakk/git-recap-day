903ad9a6aff00003c7e4a47d7f469db4752d2266
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-06 00:47:43 +0600

    git commit

--- max.js
+++ max.js
@@ -8,7 +8,46 @@
 //     }
 // }
 // whoEitTheCake(77,89,98)
-function getTheMaxNumber(num1, num2, num3){
-    console.log(Math.min(num1, num2, num3));
-}
-let maxNumber = getTheMaxNumber(77,89,98)
+// function getTheMaxNumber(num1, num2, num3){
+//     console.log(Math.min(num1, num2, num3));
+// }
+// let maxNumber = getTheMaxNumber(77,89,98)
+
+
+// problem 1
+// function getTheTallestMan(number) {
+//     let length = 0;
+//     for (let i = 0; i < number.length; i++) {
+//         const index = i
+//         const element = number[index]
+//         if (element > length) {
+//             length = element
+//         }
+//     }
+//     return length 
+// }
+// const array = [200, 120, 130, 189, 213, 215]
+// const tollest = getTheTallestMan(array)
+// console.log(tollest);
+
+
+
+
+
+// function getTheTallestMan(array) {
+//     let length = 0;
+//     for (i = 6; i < array.length; i++) {
+//         const index = i;
+//         const element = array[index];
+//         if(element > length){
+//             length = element
+//         }
+//     }
+//     return length
+// }
+// const number = [102, 120, 1, 50, 180, 9220, 23]
+// const arr = getTheTallestMan(number)
+// console.log(arr);
+
+
+// Home Worker

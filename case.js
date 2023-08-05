562ddf6cf3807143a485408019b920150e60334e
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-05 22:51:35 +0600

    git commit

---
+++ case.js
@@ -0,0 +1,9 @@
+// const userName = "blackPink"
+// const userInput = "blackPinK"
+// console.log(userName.toLocaleLowerCase());
+// console.log(userInput.toLowerCase());
+// if(userName.toLowerCase() === userInput.toLowerCase()){
+//     console.log("This is valid number");
+// }else{
+//     console.log("This is not a valid number");
+// }

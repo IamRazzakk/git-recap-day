af0cec29f57c1df81d8f3ce993301f36032fd4d7
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-05 11:57:12 +0600

    git commit

--- factorial.js
+++ factorial.js
@@ -8,12 +8,21 @@
 // }
 // const fact= facTorial(4)
 // console.log(fact);
-function factorial(number){
+// function factorial(number){
+//     result = 1
+//     for(let i = number; i>=1; i--){
+//         result = result * i;
+//     }
+//     return result
+// }
+// const fac = factorial(4)
+// console.log(fac);
+function factorial(number) {
     result = 1
-    for(let i = number; i>=1; i--){
-        result = result * i;
+    for (i = 1; i <= number; i++) {
+        result = result * i
     }
     return result
 }
-const fac = factorial(7)
-console.log(fac);
+const fact = factorial(9)
+console.log(fact);
